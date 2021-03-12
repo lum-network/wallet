e2498ebc8923c95b993c5e36bad8e2a9c7315c1f
Karim Dalaize <karim.dalaize@epitech.eu>
2021-03-12 16:51:01 +0100

    [Updated] Transactions screen table

--- src/screens/Transactions/Transactions.tsx
+++ src/screens/Transactions/Transactions.tsx
@@ -1,20 +1,26 @@
 import React from 'react';
+//import { useTranslation } from 'react-i18next';
 import { useSelector } from 'react-redux';
+
 import { RootState } from 'redux/store';
 import { TransactionsTable } from 'components';
-import { useTranslation } from 'react-i18next';
 import { Card } from 'frontend-elements';
 
 const Transactions = (): JSX.Element => {
     const transactions = useSelector((state: RootState) => state.wallet.transactions);
-    const { t } = useTranslation();
+    //const { t } = useTranslation();
 
     return (
-        <div className="p-4">
-            <h3>{t('navbar.transactions')}</h3>
-            <Card withoutPadding>
-                <TransactionsTable transactions={transactions} />
-            </Card>
+        <div className="mt-4">
+            <div className="container">
+                <div className="row gy-4">
+                    <div className="col">
+                        <Card withoutPadding>
+                            <TransactionsTable transactions={transactions} />
+                        </Card>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
