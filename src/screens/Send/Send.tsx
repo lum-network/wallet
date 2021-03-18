11840d31ec179b78f43ceba1ff3a8a1ca122f1ff
Thibault Jaillard <thibault.jaillard@gmail.com>
2021-03-18 16:23:43 +0100

    Improve Send page

--- src/screens/Send/Send.tsx
+++ src/screens/Send/Send.tsx
@@ -1,28 +1,17 @@
-import React, { PureComponent } from 'react';
-import { connect } from 'react-redux';
+import React from 'react';
+import { useSelector } from 'react-redux';
 
 import { AddressCard, BalanceCard } from 'components';
 import { Redirect } from 'react-router';
 import { RootState } from 'redux/store';
 import Button from './components/Button/Button';
-
 import assets from 'assets';
 
-interface IProps {}
+const Send = (): JSX.Element => {
+    const wallet = useSelector((state: RootState) => state.wallet.currentWallet);
+    const balance = useSelector((state: RootState) => state.wallet.currentBalance);
 
-const mapState = (state: RootState) => ({
-    wallet: state.wallet.currentWallet,
-    balance: state.wallet.currentBalance,
-});
-
-const mapDispatch = () => ({});
-
-type StateProps = ReturnType<typeof mapState>;
-type DispatchProps = ReturnType<typeof mapDispatch>;
-type Props = IProps & StateProps & DispatchProps;
-
-class Send extends PureComponent<Props> {
-    buttons = [
+    const buttons = [
         { name: 'Send', icon: assets.images.messageSend },
         { name: 'Delegate', icon: assets.images.messageDelegate },
         { name: 'Undelegate', icon: assets.images.messageUndelegate },
@@ -32,41 +21,37 @@ class Send extends PureComponent<Props> {
         { name: 'Edit Validator', icon: assets.images.messageEditValidator },
     ];
 
-    renderButtons(): JSX.Element {
+    const renderButtons = (): JSX.Element => {
         return (
             <div className="row gy-3 mt-4">
-                {this.buttons.map((button, index) => (
+                {buttons.map((button, index) => (
                     <div key={index} className="col-sm-6 col-lg-3">
                         <Button name={button.name} icon={button.icon} />
                     </div>
                 ))}
             </div>
         );
+    };
+
+    if (!wallet) {
+        return <Redirect to="/welcome" />;
     }
 
-    render(): JSX.Element {
-        const { wallet, balance } = this.props;
-
-        if (!wallet) {
-            return <Redirect to="/welcome" />;
-        }
-
-        return (
-            <div className="mt-4">
-                <div className="container">
-                    <div className="row gy-4">
-                        <div className="col-md-6">
-                            <AddressCard address={wallet.address} />
-                        </div>
-                        <div className="col-md-6">
-                            <BalanceCard balance={balance} />
-                        </div>
+    return (
+        <div className="mt-4">
+            <div className="container">
+                <div className="row gy-4">
+                    <div className="col-md-6">
+                        <AddressCard address={wallet.address} />
                     </div>
-                    {this.renderButtons()}
+                    <div className="col-md-6">
+                        <BalanceCard balance={balance} />
+                    </div>
                 </div>
+                {renderButtons()}
             </div>
-        );
-    }
-}
+        </div>
+    );
+};
 
-export default connect(mapState, mapDispatch)(Send);
+export default Send;
