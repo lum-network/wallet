c55fe44f49b4edff53650315943da03e94a43835
Karim Dalaize <karim.dalaize@epitech.eu>
2021-04-01 17:49:04 +0200

    Moved RPC Url in .env (#4)
    * [Updated] Moved RPC URL in a environment variable
    
    * [Removed] RPC Urls constant

--- src/constant/index.ts
+++ src/constant/index.ts
@@ -1,9 +1,5 @@
-export const __DEV__ = !process.env.NODE_ENV || process.env.NODE_ENV === 'development';
-
 export const LUM_WALLET_GITHUB = 'https://github.com/lum-network/wallet';
 export const LUM_MAIL = 'https://github.com/lum-network';
 export const LUM_TELEGRAM = 'https://github.com/lum-network';
 export const LUM_TWITTER = 'https://github.com/lum-network';
 export const LUM_EXPLORER = 'https://explorer.lum.network/';
-export const LUM_TESTNET = 'http://node0.testnet.lum.network/rpc';
-export const LUM_MAINNET = 'http://node0.lum.network/rpc';
