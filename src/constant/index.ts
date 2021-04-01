c3dce61cbe30aea0d8fa5930a46b710afb9ac253
Karim Dalaize <karim.dalaize@epitech.eu>
2021-04-01 17:13:17 +0200

    [Removed] RPC Urls constant

--- src/constant/index.ts
+++ src/constant/index.ts
@@ -3,5 +3,3 @@ export const LUM_MAIL = 'https://github.com/lum-network';
 export const LUM_TELEGRAM = 'https://github.com/lum-network';
 export const LUM_TWITTER = 'https://github.com/lum-network';
 export const LUM_EXPLORER = 'https://explorer.lum.network/';
-export const LUM_TESTNET = 'http://node0.testnet.lum.network/rpc';
-export const LUM_MAINNET = 'http://node0.lum.network/rpc';
