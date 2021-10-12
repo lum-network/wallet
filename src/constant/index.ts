9f15d8e107a0d905eb0437c679fad3a1d2e5ae78
Karim Dalaize <karim.dalaize@epitech.eu>
2021-10-12 17:30:22 +0200

    [Updated] wallet/explorer links domain depends on env config

--- src/constant/index.ts
+++ src/constant/index.ts
@@ -1,7 +1,10 @@
+import { IS_TESTNET } from '../utils/wallet';
+
 export const LUM_WALLET_GITHUB = 'https://github.com/lum-network/wallet';
 export const LUM_MAIL = 'contact@lum.network';
 export const LUM_TELEGRAM = 'https://t.me/lum_network';
 export const LUM_TWITTER = 'https://twitter.com/lum_network';
-export const LUM_EXPLORER = 'https://explorer.lum.network/';
+export const LUM_EXPLORER = IS_TESTNET ? 'https://explorer.testnet.lum.network' : 'https://explorer.lum.network';
+export const LUM_WALLET = IS_TESTNET ? 'https://wallet.testnet.lum.network' : 'https://wallet.lum.network';
 
 export const CLIENT_PRECISION = 1_000_000_000_000_000_000;
