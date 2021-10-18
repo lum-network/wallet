9013a8c587da252fdf1590c979abface54f8f1ba
Karim Dalaize <karim.dalaize@epitech.eu>
2021-10-18 15:04:40 +0200

    [Updated] Last transactions table translation

--- src/components/Tables/TransactionsTable.tsx
+++ src/components/Tables/TransactionsTable.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 
-import { Namespace, Resources, TFunction, useTranslation } from 'react-i18next';
+import { useTranslation } from 'react-i18next';
+import { LumConstants } from '@lum-network/sdk-javascript';
 
 import { Table } from 'frontend-elements';
 import { LUM_EXPLORER } from 'constant';
 import { Transaction, Wallet } from 'models';
 import { NumbersUtils, trunc } from 'utils';
-import { LumConstants } from '@lum-network/sdk-javascript';
 import { SmallerDecimal, TransactionTypeBadge } from 'components';
 
 interface TransactionsTableProps {
@@ -17,23 +17,23 @@ interface TransactionsTableProps {
 interface RowProps {
     row: Transaction;
     wallet: Wallet;
-    t: TFunction<Namespace<keyof Resources>>;
+    headers: string[];
 }
 
 const TransactionRow = (props: RowProps): JSX.Element => {
-    const { row, t, wallet } = props;
+    const { row, wallet, headers } = props;
 
     return (
         <tr>
-            <td data-label="Hash">
+            <td data-label={headers[0]}>
                 <a href={`${LUM_EXPLORER}/txs/${row.hash}`} target="_blank" rel="noreferrer">
                     {trunc(row.hash)}
                 </a>
             </td>
-            <td data-label="Type">
+            <td data-label={headers[1]}>
                 <TransactionTypeBadge type={row.type} userAddress={wallet.getAddress()} toAddress={row.toAddress} />
             </td>
-            <td data-label={t('transactions.table.from')}>
+            <td data-label={headers[2]}>
                 <a
                     href={`${LUM_EXPLORER}/${
                         row.fromAddress.includes(LumConstants.LumBech32PrefixValAddr) ? 'validators' : 'account'
@@ -44,7 +44,7 @@ const TransactionRow = (props: RowProps): JSX.Element => {
                     {trunc(row.fromAddress)}
                 </a>
             </td>
-            <td data-label={t('transactions.table.to')} className="text-end">
+            <td data-label={headers[3]} className="text-end">
                 <a
                     href={`${LUM_EXPLORER}/${
                         row.toAddress.includes(LumConstants.LumBech32PrefixValAddr) ? 'validators' : 'account'
@@ -55,7 +55,7 @@ const TransactionRow = (props: RowProps): JSX.Element => {
                     {trunc(row.toAddress)}
                 </a>
             </td>
-            <td data-label={t('transactions.table.amount')} className="text-end">
+            <td data-label={headers[4]} className="text-end">
                 <SmallerDecimal
                     nb={NumbersUtils.formatUnit(
                         row.amount && row.amount[0]
@@ -69,7 +69,7 @@ const TransactionRow = (props: RowProps): JSX.Element => {
                 />
                 <span className="ms-2">{LumConstants.LumDenom}</span>
             </td>
-            <td data-label="Time" className="text-end">
+            <td data-label={headers[5]} className="text-end">
                 <div className="text-truncate">{row.time}</div>
             </td>
         </tr>
@@ -86,7 +86,7 @@ const TransactionsTable = (props: TransactionsTableProps): JSX.Element => {
             t('transactions.table.from'),
             t('transactions.table.to'),
             t('transactions.table.amount'),
-            'Time',
+            t('transactions.table.time'),
         ];
 
         const txs = [...props.transactions].sort((txA, txB) => (txA.height < txB.height ? 1 : -1));
@@ -94,7 +94,7 @@ const TransactionsTable = (props: TransactionsTableProps): JSX.Element => {
         return (
             <Table head={headers}>
                 {txs.map((tx, index) => (
-                    <TransactionRow key={index} row={tx} t={t} wallet={props.wallet} />
+                    <TransactionRow key={index} row={tx} wallet={props.wallet} headers={headers} />
                 ))}
             </Table>
         );
