d12bbb6b3e1828c12b3566694287f368af39a045
Segfault <5221072+Segfaultd@users.noreply.github.com>
2021-11-05 11:40:30 +0100

    Bring changes to production (#24)

--- src/constant/index.ts
+++ src/constant/index.ts
@@ -8,6 +8,7 @@ export const LUM_LEDGER_APP_INSTALL_LINK =
     'https://github.com/lum-network/ledger-app#download-and-install-a-prerelease';
 export const COSMOS_LEDGER_APP_INSTALL_LINK =
     'https://support.ledger.com/hc/en-us/articles/360013713840-Cosmos-ATOM-?docs=true';
+export const KEPLR_INSTALL_LINK = 'https://keplr.app';
 export const LUM_EXPLORER = IS_TESTNET ? 'https://explorer.testnet.lum.network' : 'https://explorer.lum.network';
 export const LUM_WALLET = IS_TESTNET ? 'https://wallet.testnet.lum.network' : 'https://wallet.lum.network';
 
