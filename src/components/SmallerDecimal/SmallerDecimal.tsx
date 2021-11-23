8972e382b32f1a513f435952b9769dbd40af2c09
Karim Dalaize <karim.dalaize@epitech.eu>
2021-11-23 17:52:18 +0100

    [Updated] Harmonized amounts display

--- src/components/SmallerDecimal/SmallerDecimal.tsx
+++ src/components/SmallerDecimal/SmallerDecimal.tsx
@@ -3,21 +3,22 @@ import './SmallerDecimal.scss';
 
 interface IProps {
     nb: string;
+    big?: boolean;
 }
 
-const SmallerDecimal = ({ nb }: IProps): JSX.Element => {
+const SmallerDecimal = ({ nb, big }: IProps): JSX.Element => {
     const split = nb.split('.');
 
     if (split.length > 1) {
         return (
-            <span className="smaller-decimal">
+            <span className={big ? 'smaller-decimal-big' : 'smaller-decimal'}>
                 {split[0]}
                 <small>.{split[1]}</small>
             </span>
         );
     }
 
-    return <span>{nb}</span>;
+    return big ? <h1 className="display-6 fw-normal">{nb}</h1> : <span>{nb}</span>;
 };
 
 export default SmallerDecimal;
