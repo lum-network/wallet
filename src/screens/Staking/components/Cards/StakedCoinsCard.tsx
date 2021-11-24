fb3307d9575c569ba49919d82aa6a929750c8fa8
Karim Dalaize <karim.dalaize@epitech.eu>
2021-11-24 15:09:01 +0100

    [LUM-289] Wallet -> Harmonize amounts display (#32)
    * [Updated] Harmonized amounts display
    
    * [Updated] 0 number display

--- src/screens/Staking/components/Cards/StakedCoinsCard.tsx
+++ src/screens/Staking/components/Cards/StakedCoinsCard.tsx
@@ -1,7 +1,9 @@
 import assets from 'assets';
+import { SmallerDecimal } from 'components';
 import { Card } from 'frontend-elements';
 import React from 'react';
 import { useTranslation } from 'react-i18next';
+import { NumbersUtils } from 'utils';
 
 import './styles/Cards.scss';
 
@@ -12,7 +14,9 @@ const StakedCoinsCard = ({ amount }: { amount: number }): JSX.Element => {
         <Card withoutPadding className="h-100 dashboard-card justify-content-start staked-coins-card p-4">
             <h2 className="ps-2 pt-3 text-white">{t('staking.stakedCoins')}</h2>
             <div className="ps-2 my-3 d-flex flex-row align-items-baseline w-100">
-                <h1 className="display-6 fw-normal me-2 me-sm-3 text-white text-truncate">{amount}</h1>
+                <div className="me-2 me-sm-3 text-white text-truncate">
+                    <SmallerDecimal nb={NumbersUtils.formatTo6digit(amount)} big />
+                </div>
                 <img src={assets.images.lumTicker} className="ticker" />
             </div>
         </Card>
