8a071c00d4c06eecefb1613f88bc48a8b0487634
Karim Dalaize <karim.dalaize@epitech.eu>
2021-11-30 17:21:17 +0100

    [Updated] Same number display for vested tokens card

--- src/screens/Staking/components/Cards/VestedTokensCard.tsx
+++ src/screens/Staking/components/Cards/VestedTokensCard.tsx
@@ -5,6 +5,7 @@ import assets from 'assets';
 import { Card } from 'frontend-elements';
 import { Vestings } from 'models';
 import { dateToNow, NumbersUtils } from 'utils';
+import { SmallerDecimal } from 'components';
 
 const VestedTokensCard = ({ vestings }: { vestings: Vestings }): JSX.Element => {
     const { t } = useTranslation();
@@ -13,9 +14,9 @@ const VestedTokensCard = ({ vestings }: { vestings: Vestings }): JSX.Element => 
         <Card withoutPadding className="h-100 dashboard-card justify-content-start vested-tokens-card p-4">
             <h2 className="ps-2 pt-3 text-white">{t('staking.vestedTokens')}</h2>
             <div className="ps-2 my-3 d-flex flex-row align-items-baseline w-100">
-                <h1 className="display-6 fw-normal me-2 me-sm-3 text-white text-truncate">
-                    {NumbersUtils.formatUnit(vestings.lockedCoins, true)}
-                </h1>
+                <div className="me-2 me-sm-3 text-white text-truncate">
+                    <SmallerDecimal nb={NumbersUtils.formatUnit(vestings.lockedCoins, true)} big />
+                </div>
                 <img src={assets.images.lumTicker} className="ticker" />
             </div>
             <p className="align-self-end text-white">
