244f045da7fccda40e73ab6aad71839adee68e47
Karim Dalaize <karim.dalaize@epitech.eu>
2023-06-05 18:25:25 +0200

    Revert "[Updated] Dependencies and fixes related"
    This reverts commit 164c3f103b13fbc5e643ded869a5a2391987cb99 and fix build/test.

--- config-overrides.js
+++ config-overrides.js
@@ -1,22 +1,42 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
+/* eslint-disable @typescript-eslint/no-var-requires */
 const webpack = require('webpack');
+const path = require('path');
 
-module.exports = function override(config) {
-    //do stuff with the webpack config...
-    config.resolve.fallback = {
-        ...config.resolve.fallback,
-        path: require.resolve('path-browserify'),
-        stream: require.resolve('stream-browserify'),
-        crypto: require.resolve('crypto-browserify'),
-        querystring: require.resolve('querystring-es3'),
-        buffer: require.resolve('buffer'),
-    };
-    config.plugins = [
-        ...config.plugins,
-        new webpack.ProvidePlugin({
-            Buffer: ['buffer', 'Buffer'],
-        }),
-    ];
+module.exports = {
+    webpack: function (config) {
+        //do stuff with the webpack config...
+        config.resolve.fallback = {
+            ...config.resolve.fallback,
+            path: require.resolve('path-browserify'),
+            stream: require.resolve('stream-browserify'),
+            crypto: require.resolve('crypto-browserify'),
+            querystring: require.resolve('querystring-es3'),
+            buffer: require.resolve('buffer'),
+        };
+        config.resolve.modules = [path.resolve(__dirname, 'src'), 'node_modules'];
+        config.plugins = [
+            ...config.plugins,
+            new webpack.ProvidePlugin({
+                Buffer: ['buffer', 'Buffer'],
+            }),
+        ];
 
-    return config;
+        return config;
+    },
+    jest: function (config) {
+        config.transform = {
+            ...config.transform,
+            '^.+\\.(ts|tsx)$': 'ts-jest',
+        };
+
+        config.transformIgnorePatterns = ['node_modules/(?!(axios))'];
+
+        config.moduleNameMapper = {
+            ...config.moduleNameMapper,
+            '\\.(css|scss)$': 'identity-obj-proxy',
+            '@ledgerhq/devices': '@ledgerhq/devices/lib',
+        };
+
+        return config;
+    },
 };
