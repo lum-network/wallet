324e7fe0197816fcd37affd8967f2ddef8444ab8
Karim Dalaize <karim.dalaize@epitech.eu>
2023-06-05 16:28:57 +0200

    [Removed] unused report function (deprecation of usage)

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import reportWebVitals from './reportWebVitals';
 
 import App from './App';
 
@@ -14,8 +13,3 @@ const container = document.getElementById('root');
 const root = createRoot(container!);
 
 root.render(<App />);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
